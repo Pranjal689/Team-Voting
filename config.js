c7f9bda492c32a1608e7181f566c052379619159
Pranjal689 <76397224+Pranjal689@users.noreply.github.com>
2021-10-13 10:38:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyA-PSSQYIfQHP-3MpGWyUgzwUHbrdTqbGw",
+  authDomain: "voting-9063a.firebaseapp.com",
+  projectId: "voting-9063a",
+  storageBucket: "voting-9063a.appspot.com",
+  messagingSenderId: "258820387892",
+  appId: "1:258820387892:web:e0fda03bb57a65a09b4661"
+};
 
 };
 // Initialize Firebase
